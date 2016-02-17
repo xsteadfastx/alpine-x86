21f264f8bffd3dc2f1a9384268156d1bfadbf6df
Marvin Steadfast <marvin@xsteadfastx.org>
2016-02-17 11:55:30 +0100

    fix version numbering

--- build.sh
+++ build.sh
@@ -13,6 +13,8 @@ mkdir $TMPDIR
 docker build -t alpine-builder builder
 docker run -e "USERID=$UID" -v $TMPDIR:/data --rm alpine-builder $VERSION
 docker rmi alpine-builder
+
+VERSION=$(echo $VERSION | sed 's/v\([0-9].[0-9]\)/\1/')
 cat $TMPDIR/rootfs.tar.gz | docker import - xsteadfastx/alpine-x86:$VERSION
 
 rm -rf $TMPDIR
--- builder/scripts/mkimage-alpine.sh
+++ builder/scripts/mkimage-alpine.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-MIRROR=http://nl.alpinelinux.org/alpine/
+MIRROR=http://nl.alpinelinux.org/alpine
 ROOTFS="/tmp/rootfs"
 VERSION=$1
 
@@ -11,13 +11,13 @@ wget "$MIRROR/latest-stable/main/x86/apk-tools-static-2.6.5-r1.apk"
 tar -xzf apk-tools-static-2.6.5-r1.apk
 
 # CREATE CHROOT
-./sbin/apk.static -X $MIRROR/v$VERSION/main -U --allow-untrusted --root $ROOTFS --initdb add alpine-base
+./sbin/apk.static -X $MIRROR/$VERSION/main -U --allow-untrusted --root $ROOTFS --initdb add alpine-base
 rm -f "$ROOTFS/var/cache/apk"/*
 
 # REPOS
 mkdir -p "$ROOTFS/etc/apk"
-echo "$MIRROR/v$VERSION/main" > "$ROOTFS/etc/apk/repositories"
-echo "$MIRROR/v$VERSION/community" >> "$ROOTFS/etc/apk/repositories"
+echo "$MIRROR/$VERSION/main" > "$ROOTFS/etc/apk/repositories"
+echo "$MIRROR/$VERSION/community" >> "$ROOTFS/etc/apk/repositories"
 
 tar -z -f rootfs.tar.gz --numeric-owner -C "$ROOTFS" -c .
 mv rootfs.tar.gz /data
